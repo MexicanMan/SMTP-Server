bb86dee05bb2f3b45ed62986127e4d1ca735c72f
Nikita Nazarenko <sombrero13nn@gmail.com>
2020-12-02 17:59:40 +0300

    Server moved to make

--- build_debug.sh
+++ build_debug.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-cd SMTPServer 
-cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_DEBUG=ON . || exit 1
-make || exit 2
+cd SMTPServer
+mkdir -p build
+make exe_directory="../../" \
+    compile_flags="-g -g3 -O0 -D_GNU_SOURCE -DDEBUG" \
+    --directory=build \
+    --makefile=../Makefile || exit 1
  
 cd ../SMTPClient
 cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_DEBUG=ON . || exit 3
